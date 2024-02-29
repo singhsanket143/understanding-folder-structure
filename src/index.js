bf94db33b164bf9dc6a6a2033ff257f42adad5e0
Sanket Singh <singhsanket143@gmail.com>
2024-02-29 22:54:46 +0530

    added codes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const { PORT, EMAIL } = require('./config/server.config');
+const { PORT } = require('./config/server.config');
+const router = require('./routes');
+const customRouter = require('./routes/customrouter');
 
 const app = express();
+
+app.use('/api', router); // localhost:3000/api/home
+app.use('/custom',customRouter); // localhost:3000/custom/custom2
 
 
 app.listen(PORT, () => {
     console.log(`Started server at port: ${PORT}`);
 });
 
+/**
+ * How will u manage these kind of routes using express router ? 
+ * 
+ * 
+ * localhost:3000/api/v1/products/:id
+ * localhost:3000/api/v1/categories/:id
+ * localhost:3000/api/v2/products/:id
+ *  localhost:3000/api/v2/categories/:id
+ * localhost:3000/api/v2/users/:id
+ */
